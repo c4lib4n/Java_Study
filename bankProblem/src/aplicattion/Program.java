870e66b1b460c47c6c3cabe9bc023e3d9d9ff384
Matheus <matheussilva_51@hotmail.com>
2023-10-30 20:20:12 +0000

    Bank project after correction!!

--- bankProblem/src/aplicattion/Program.java
+++ bankProblem/src/aplicattion/Program.java
@@ -20,49 +20,37 @@ public class Program {
         String name = sc.nextLine();
 
         System.out.println("Do you want to add money to your account? (y/n)");
-        User userAccount = new User(name, account);
+        User userAccount;
 
-        double amount = 0;
+        double amount;
         if (sc.nextLine().equals("y")) {
             System.out.println("Enter amount: ");
             amount = sc.nextDouble();
-            userAccount.setAmount(amount);
+            userAccount = new User(name, account, amount);
         } else {
-            userAccount.setAmount(0);
+            userAccount = new User(name, account);
         }
 
         System.out.println("User Data: ");
-        System.out.println("User Account: " + userAccount.getAccount() + ", Holder: " + userAccount.getName() + ", Balance: " + userAccount.getAmount());
+        System.out.println(userAccount);
 
         System.out.println("Enter a deposit: ");
         amount = sc.nextDouble();
 
         userAccount.addMoney(amount);
 
-        System.out.println("User Account: " + userAccount.getAccount() + ", Holder: " + userAccount.getName() + ", Balance: " + userAccount.getAmount());
+        System.out.println(userAccount);
 
         System.out.println("Enter a withdrawal: ");
         amount = sc.nextDouble();
 
         userAccount.removeMoney(amount);
 
-        System.out.println("User Account: " + userAccount.getAccount() + ", Holder: " + userAccount.getName() + ", Balance: " + userAccount.getAmount());
+        System.out.println(userAccount);
 
 
 
         sc.close();
-    //add comments to test the push //add comments to test the push
-        //add comments to test the push
-        //add comments to test the push //add comments to test the push
-        //add comments to test the push
-        //add comments to test the push
-        //add comments to test the push
-        //add comments to test the push
-        //add comments to test the push
-        //add comments to test the push
-        //add comments to test the push
-        //add comments to test the push
-
 
 
     }
