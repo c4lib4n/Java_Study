0c3bf82e809bab8ee677fee4471b60f5c410aadc
Matheus <matheussilva_51@hotmail.com>
2023-10-31 18:07:53 +0000

    Add function array average height

--- Arrays/src/aplication/Program.java
+++ Arrays/src/aplication/Program.java
@@ -1,7 +1,29 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.UK);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double [] vect = new double[n];
+
+        for(int i = 0; i <n ; i++){
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0.0;
+        for(int i = 0; i<n; i++){
+            sum += vect[i];
+        }
+
+        double avg = sum / n;
+
+        System.out.printf("Avarege height: %.2f%n", avg);
 
     }
 }
