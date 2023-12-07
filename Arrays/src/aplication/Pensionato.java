1bf24437dcc7d45f678a85eafb9036e74a600a20
matheusSilva <matheussilva_51@hotmail.com>
2023-12-07 21:22:55 +0000

    ADD a little be more of the exercise.

--- Arrays/src/aplication/Pensionato.java
+++ Arrays/src/aplication/Pensionato.java
@@ -1,7 +1,33 @@
 package aplication;
 
-import entities.Rent;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Pensionato {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.UK);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many rooms will be rent? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <=n; i++){
+            System.out.println();
+            System.out.println("Rent #" + i + ":");
+            System.out.println("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.println("Email: ");
+            String email = sc.nextLine();
+            System.out.println("Room: ");
+            int numRoom = sc.nextInt();
+
+        }
+
+
+
+    }
+
 
 
 
