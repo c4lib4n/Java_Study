78668b6641c5f0995afa1cfbe0e0e6f898396487
matheusSilva <matheussilva_51@hotmail.com>
2023-12-11 16:44:49 +0000

    Exercises about list!!

--- Arrays/src/aplication/Pensionato.java
+++ Arrays/src/aplication/Pensionato.java
@@ -1,5 +1,7 @@
 package aplication;
 
+import entities.Rent;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -11,6 +13,8 @@ public class Pensionato {
         System.out.println("How many rooms will be rent? ");
         int n = sc.nextInt();
 
+        Rent[] vect = new Rent[10];
+
         for (int i = 1; i <=n; i++){
             System.out.println();
             System.out.println("Rent #" + i + ":");
@@ -20,10 +24,19 @@ public class Pensionato {
             System.out.println("Email: ");
             String email = sc.nextLine();
             System.out.println("Room: ");
-            int numRoom = sc.nextInt();
+            int roomNumber = sc.nextInt();
 
+            Rent rent = new Rent(name, email);
+            vect[roomNumber] = new Rent(name, email);
         }
 
+        System.out.println();
+        System.out.println("Busy rooms!");
+        for (int i = 0; i < 10; i++){
+             if (vect[i] != null){
+                 System.out.println(i + ": " + vect[i]);
+             }
+        }
 
 
     }
